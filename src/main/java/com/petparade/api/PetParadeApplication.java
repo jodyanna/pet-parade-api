91f50e29401be1c29f2a0a1c0192a5ec6920cf23
Joseph D'Anna <jodyanna14.15@gmail.com>
2021-09-19 10:38:50 -0500

    first commit

---
+++ src/main/java/com/petparade/api/PetParadeApplication.java
@@ -0,0 +1,13 @@
+package com.petparade.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetParadeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PetParadeApplication.class, args);
+  }
+
+}
