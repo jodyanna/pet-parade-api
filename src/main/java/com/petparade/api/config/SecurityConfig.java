72ba1b41936f4028134538b92e978fdd35ce2fe2
Joseph D'Anna <jodyanna14.15@gmail.com>
2021-09-29 13:50:53 -0500

    started security config, user details

---
+++ src/main/java/com/petparade/api/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.petparade.api.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+  @Autowired
+  UserDetailsService userDetailsService;
+
+  @Override
+  public void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService);
+  }
+
+  @Override
+  public void configure(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity.authorizeRequests()
+        .antMatchers("/users").hasRole("USER")
+        .antMatchers("/pets").hasRole("USER")
+        .antMatchers("/species").permitAll()
+        .antMatchers("/").permitAll()
+        .and().formLogin();
+  }
+
+  @Bean
+  public PasswordEncoder getPasswordEncoder() {
+    return NoOpPasswordEncoder.getInstance();
+  }
+}
