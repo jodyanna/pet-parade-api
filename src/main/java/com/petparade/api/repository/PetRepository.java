5e15a9189ad1e071ce11afb1984973cea03e5c66
Joseph D'Anna <jodyanna14.15@gmail.com>
2021-10-13 11:20:07 -0500

    bugfix with querying recently created pets

--- src/main/java/com/petparade/api/repository/PetRepository.java
+++ src/main/java/com/petparade/api/repository/PetRepository.java
@@ -16,6 +16,6 @@ public interface PetRepository extends JpaRepository<Pet, Long> {
   @Query(value = "SELECT p FROM Pet AS p WHERE p.species.id=:id")
   Set<Pet> getPetsBySpecies(@Param("id") Long id);
 
-  @Query(value = "SELECT * FROM `Pets` AS p ORDER BY `dateCreated` LIMIT 2;", nativeQuery = true)
+  @Query(value = "SELECT * FROM `Pets` AS p ORDER BY `id` DESC LIMIT 2;", nativeQuery = true)
   Set<Pet> getRecentCreatedPets();
 }
