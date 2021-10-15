85d45765736cef18a23d0baac5a82068fd4565c9
Joseph D'Anna <jodyanna14.15@gmail.com>
2021-10-15 12:00:39 -0500

    added sql files for setting up database

---
+++ src/main/resources/db-init.sql
@@ -0,0 +1,103 @@
+
+-- Create PetParade schema
+CREATE SCHEMA `PetParade` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
+
+-- Create User table 
+CREATE TABLE `PetParade`.`Users` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  `city` VARCHAR(45) NULL,
+  `state` VARCHAR(45) NULL,
+  `dateCreated` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dateModified` DATETIME NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);
+  
+-- Create Roles table
+CREATE TABLE `PetParade`.`Roles` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `role` VARCHAR(45) NULL,
+  `FK_Roles_Users` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `FK_Roles_Users_idx` (`FK_Roles_Users` ASC) VISIBLE,
+  CONSTRAINT `FK_Roles_Users`
+    FOREIGN KEY (`FK_Roles_Users`)
+    REFERENCES `PetParade`.`Users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+  
+-- Create Species table
+CREATE TABLE `PetParade`.`Species` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE);
+
+-- Create Pets table
+CREATE TABLE `PetParade`.`Pets` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `bio` VARCHAR(255) NULL,
+  `birthday` DATETIME NULL,
+  `isFlagged` TINYINT NULL,
+  `dateCreated` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dateModified` DATETIME NULL,
+  `image` NVARCHAR(260) NULL,
+  `FK_Pets_Users` INT UNSIGNED NOT NULL,
+  `FK_Pets_Species` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `FK_Pets_Users_idx` (`FK_Pets_Users` ASC) VISIBLE,
+  INDEX `FK_Pets_Species_idx` (`FK_Pets_Species` ASC) VISIBLE,
+  CONSTRAINT `FK_Pets_Users`
+    FOREIGN KEY (`FK_Pets_Users`)
+    REFERENCES `PetParade`.`Users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `FK_Pets_Species`
+    FOREIGN KEY (`FK_Pets_Species`)
+    REFERENCES `PetParade`.`Species` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+    
+-- Create Likes table
+CREATE TABLE `PetParade`.`Likes` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `dateCreated` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `FK_Likes_Pets` INT UNSIGNED NOT NULL,
+  `FK_Likes_Users` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `FK_Likes_Pets_idx` (`FK_Likes_Pets` ASC) VISIBLE,
+  INDEX `FK_Likes_Users_idx` (`FK_Likes_Users` ASC) VISIBLE,
+  CONSTRAINT `FK_Likes_Pets`
+    FOREIGN KEY (`FK_Likes_Pets`)
+    REFERENCES `PetParade`.`Pets` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `FK_Likes_Users`
+    FOREIGN KEY (`FK_Likes_Users`)
+    REFERENCES `PetParade`.`Users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+    
+-- Create Ratings table
+CREATE TABLE `PetParade`.`Ratings` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `rating` INT NOT NULL,
+  `dateCreated` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `FK_Ratings_Users` INT UNSIGNED NOT NULL,
+  `FK_Ratings_Pets` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `FK_Ratings_Users_idx` (`FK_Ratings_Users` ASC) VISIBLE,
+  INDEX `FK_Ratings_Pets_idx` (`FK_Ratings_Pets` ASC) VISIBLE,
+  CONSTRAINT `FK_Ratings_Users`
+    FOREIGN KEY (`FK_Ratings_Users`)
+    REFERENCES `PetParade`.`Users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `FK_Ratings_Pets`
+    FOREIGN KEY (`FK_Ratings_Pets`)
+    REFERENCES `PetParade`.`Pets` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
---
+++ src/main/resources/insert-stuff.sql
@@ -0,0 +1,151 @@
+
+-- Fake species
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ("DOG");
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ("CAT");
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ("BIRD");
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ("FISH");
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ("GOAT");
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ("PIG");
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ("SHEEP");
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ("COW");
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ("DONKEY");
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ("HORSE");
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ("FERRET");
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ("RABBIT");
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ("RODENT");
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ("INSECT");
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ("REPTILE");
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ("FOX");
+
+INSERT INTO `PetParade`.`Species`
+(`name`)
+VALUES ("OTHER");
+
+-- Insert fake user data
+-- Fake user id=1
+INSERT INTO `PetParade`.`Users`
+(`username`, `email`, `password`)
+VALUES ("admin", "admin@email.com", "password");
+
+INSERT INTO `PetParade`.`Roles`
+(`role`, `FK_Roles_Users`)
+VALUES ("ROLE_USER", 1);
+
+INSERT INTO `PetParade`.`Roles`
+(`role`, `FK_Roles_Users`)
+VALUES ("ROLE_ADMIN", 1);
+
+INSERT INTO `PetParade`.`Pets`
+(`name`, `bio`, `birthday`, `isFlagged`, `FK_Pets_Users`, `FK_Pets_Species`)
+VALUES ("Gus", "Itchy dog that loves scratching!", "2009-09-09", 0, 1, 1);
+
+INSERT INTO `PetParade`.`Pets`
+(`name`, `bio`, `birthday`, `isFlagged`, `FK_Pets_Users`, `FK_Pets_Species`)
+VALUES ("Tag", "Example dog bio would go here", "2004-04-04", 0, 1, 1);
+
+-- Fake user id=2
+INSERT INTO `PetParade`.`Users`
+(`username`, `email`, `password`)
+VALUES ("test", "test@email.com", "password");
+
+INSERT INTO `PetParade`.`Roles`
+(`role`, `FK_Roles_Users`)
+VALUES ("ROLE_USER", 2);
+
+INSERT INTO `PetParade`.`Pets`
+(`name`, `bio`, `birthday`, `isFlagged`, `FK_Pets_Users`, `FK_Pets_Species`)
+VALUES ("Cat", "Meow meow meow meow meow", "2009-09-09", 0, 2, 2);
+
+-- Fake user id=3
+INSERT INTO `PetParade`.`Users`
+(`username`, `email`, `password`)
+VALUES ("test2", "test2@email.com", "password");
+
+INSERT INTO `PetParade`.`Roles`
+(`role`, `FK_Roles_Users`)
+VALUES ("ROLE_USER", 3);
+
+INSERT INTO `PetParade`.`Pets`
+(`name`, `bio`, `birthday`, `isFlagged`, `FK_Pets_Users`, `FK_Pets_Species`)
+VALUES ("Sebastian", "I'm actually a crab", "2009-09-09", 0, 3, 2);
+
+-- Fake likes
+INSERT INTO `PetParade`.`Likes`
+(`FK_Likes_Users`, `FK_Likes_Pets`)
+VALUES (1, 3);
+
+INSERT INTO `PetParade`.`Likes`
+(`FK_Likes_Users`, `FK_Likes_Pets`)
+VALUES (3, 3);
+
+INSERT INTO `PetParade`.`Likes`
+(`FK_Likes_Users`, `FK_Likes_Pets`)
+VALUES (2, 1);
+
+INSERT INTO `PetParade`.`Likes`
+(`FK_Likes_Users`, `FK_Likes_Pets`)
+VALUES (2, 2);
+
+-- Fake ratings
+INSERT INTO `PetParade`.`Ratings`
+(`rating`, `FK_Ratings_Users`, `FK_Ratings_Pets`)
+VALUES (3, 1, 3);
+
+INSERT INTO `PetParade`.`Ratings`
+(`rating`, `FK_Ratings_Users`, `FK_Ratings_Pets`)
+VALUES (4, 2, 1);
+
+INSERT INTO `PetParade`.`Ratings`
+(`rating`, `FK_Ratings_Users`, `FK_Ratings_Pets`)
+VALUES (4, 2, 2);
+
+INSERT INTO `PetParade`.`Ratings`
+(`rating`, `FK_Ratings_Users`, `FK_Ratings_Pets`)
+VALUES (4, 1, 3);
